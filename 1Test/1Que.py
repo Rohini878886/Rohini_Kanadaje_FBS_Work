8024457bf97210f34fe3ee7a09cb88bf4fe152a5
Rohini kailas kanadaje <rohinikandaje201@gmail.com>
2025-08-22 19:22:35 +0530

    Update 1Que.py

--- 1Test/1Que.py
+++ 1Test/1Que.py
@@ -1,8 +1,11 @@
-l = int(input("enetr the length of perimeter:"))
-b = int(input("enetr the breadth of perimeter:"))
-r = int(input("enter the radius of perimeter:"))
-  
-pi = 3.14  
-peri = pi * r + 2 * l + r
+l = int(input("enetr the length :"))
+b = int(input("enetr the breadth :"))
+r = int(input("enter the radius:"))
+pi = 3.14
+area_of_rectangle = l * b
+area_of_semicircle = 0.5 * pi * r**2
+total_area = area_of_rectangle + area_of_semicircle  
+peri = (2 * l) + b + (pi * r )
 
+print(f"area of figure is:{total_area}")
 print(f"perimeter of figure is:{peri}")
